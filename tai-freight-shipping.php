7d4b8c194e594a65bd8a78c24cc2358cc17d5015
Michaelgb13 <mbennett@bennettnbennett.com>
2026-04-14 11:06:29 -0400

    NMFC, UN, Hazardous Material, Packaging Type, Packing Group added to UI

--- tai-freight-shipping.php
+++ tai-freight-shipping.php
@@ -69,6 +69,14 @@ function tai_freight_init() {
 
     // Enqueue admin styles.
     add_action( 'admin_enqueue_scripts', 'tai_freight_admin_styles' );
+
+    // Product-level freight data fields.
+    add_action( 'woocommerce_product_options_shipping', 'tai_freight_render_product_shipping_fields' );
+    add_action( 'woocommerce_admin_process_product_object', 'tai_freight_save_product_shipping_fields' );
+
+    // Variation-level freight data fields.
+    add_action( 'woocommerce_variation_options_shipping', 'tai_freight_render_variation_shipping_fields', 10, 3 );
+    add_action( 'woocommerce_save_product_variation', 'tai_freight_save_variation_shipping_fields', 10, 2 );
 }
 
 /**
@@ -110,6 +118,201 @@ function tai_freight_admin_styles( $hook ) {
         array(),
         TAI_FREIGHT_VERSION
     );
+}
+
+/**
+ * Render TAI freight fields on the WooCommerce product Shipping tab.
+ */
+function tai_freight_render_product_shipping_fields() {
+    echo '<div class="options_group">';
+
+    woocommerce_wp_text_input( array(
+        'id'          => '_tai_nmfc',
+        'label'       => __( 'NMFC', 'tai-freight-shipping' ),
+        'description' => __( 'National Motor Freight Classification code for this product.', 'tai-freight-shipping' ),
+        'desc_tip'    => true,
+        'placeholder' => '156600',
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'          => '_tai_un_number',
+        'label'       => __( 'UN Number', 'tai-freight-shipping' ),
+        'description' => __( 'UN hazardous material identification number (if applicable).', 'tai-freight-shipping' ),
+        'desc_tip'    => true,
+        'placeholder' => 'UN1993',
+    ) );
+
+    woocommerce_wp_checkbox( array(
+        'id'          => '_tai_hazardous_material',
+        'label'       => __( 'Hazardous Material', 'tai-freight-shipping' ),
+        'description' => __( 'Mark this product as hazardous for freight rating.', 'tai-freight-shipping' ),
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'                => '_tai_packaging_type',
+        'label'             => __( 'Packaging Type', 'tai-freight-shipping' ),
+        'description'       => __( 'TAI PackagingType enum value for this product.', 'tai-freight-shipping' ),
+        'desc_tip'          => true,
+        'type'              => 'number',
+        'custom_attributes' => array(
+            'min'  => '0',
+            'step' => '1',
+        ),
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'                => '_tai_packing_group',
+        'label'             => __( 'Packing Group', 'tai-freight-shipping' ),
+        'description'       => __( 'TAI PackingGroup enum value for this product.', 'tai-freight-shipping' ),
+        'desc_tip'          => true,
+        'type'              => 'number',
+        'custom_attributes' => array(
+            'min'  => '0',
+            'step' => '1',
+        ),
+    ) );
+
+    echo '</div>';
+}
+
+/**
+ * Save TAI freight product fields.
+ *
+ * @param WC_Product $product Product object being saved.
+ */
+function tai_freight_save_product_shipping_fields( $product ) {
+    if ( ! $product instanceof WC_Product ) {
+        return;
+    }
+
+    $nmfc = isset( $_POST['_tai_nmfc'] ) ? sanitize_text_field( wp_unslash( $_POST['_tai_nmfc'] ) ) : '';
+    $product->update_meta_data( '_tai_nmfc', $nmfc );
+
+    $un_number = isset( $_POST['_tai_un_number'] ) ? sanitize_text_field( wp_unslash( $_POST['_tai_un_number'] ) ) : '';
+    $product->update_meta_data( '_tai_un_number', $un_number );
+
+    $hazardous_material = isset( $_POST['_tai_hazardous_material'] ) ? 'yes' : 'no';
+    $product->update_meta_data( '_tai_hazardous_material', $hazardous_material );
+
+    $packaging_type = isset( $_POST['_tai_packaging_type'] ) ? absint( wp_unslash( $_POST['_tai_packaging_type'] ) ) : '';
+    $product->update_meta_data( '_tai_packaging_type', ( '' === $packaging_type ) ? '' : (string) $packaging_type );
+
+    $packing_group = isset( $_POST['_tai_packing_group'] ) ? absint( wp_unslash( $_POST['_tai_packing_group'] ) ) : '';
+    $product->update_meta_data( '_tai_packing_group', ( '' === $packing_group ) ? '' : (string) $packing_group );
+}
+
+/**
+ * Render TAI freight fields for each product variation.
+ *
+ * @param int              $loop           Variation index.
+ * @param array            $variation_data Variation data.
+ * @param WP_Post|WC_Product_Variation $variation Variation object.
+ */
+function tai_freight_render_variation_shipping_fields( $loop, $variation_data, $variation ) {
+    $variation_id = is_object( $variation ) && isset( $variation->ID ) ? absint( $variation->ID ) : 0;
+
+    if ( $variation_id <= 0 ) {
+        return;
+    }
+
+    $nmfc               = get_post_meta( $variation_id, '_tai_nmfc', true );
+    $un_number          = get_post_meta( $variation_id, '_tai_un_number', true );
+    $hazardous_material = get_post_meta( $variation_id, '_tai_hazardous_material', true );
+    $packaging_type     = get_post_meta( $variation_id, '_tai_packaging_type', true );
+    $packing_group      = get_post_meta( $variation_id, '_tai_packing_group', true );
+
+    woocommerce_wp_text_input( array(
+        'id'            => "_tai_nmfc_variation_{$loop}",
+        'name'          => '_tai_nmfc_variation[' . $loop . ']',
+        'label'         => __( 'TAI NMFC', 'tai-freight-shipping' ),
+        'value'         => $nmfc,
+        'desc_tip'      => true,
+        'description'   => __( 'NMFC code for this variation.', 'tai-freight-shipping' ),
+        'wrapper_class' => 'form-row form-row-full',
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'            => "_tai_un_number_variation_{$loop}",
+        'name'          => '_tai_un_number_variation[' . $loop . ']',
+        'label'         => __( 'TAI UN Number', 'tai-freight-shipping' ),
+        'value'         => $un_number,
+        'desc_tip'      => true,
+        'description'   => __( 'UN number for this variation (if hazardous).', 'tai-freight-shipping' ),
+        'wrapper_class' => 'form-row form-row-full',
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'                => "_tai_packaging_type_variation_{$loop}",
+        'name'              => '_tai_packaging_type_variation[' . $loop . ']',
+        'label'             => __( 'TAI Packaging Type', 'tai-freight-shipping' ),
+        'value'             => $packaging_type,
+        'type'              => 'number',
+        'desc_tip'          => true,
+        'description'       => __( 'PackagingType enum value for this variation.', 'tai-freight-shipping' ),
+        'wrapper_class'     => 'form-row form-row-first',
+        'custom_attributes' => array(
+            'min'  => '0',
+            'step' => '1',
+        ),
+    ) );
+
+    woocommerce_wp_text_input( array(
+        'id'                => "_tai_packing_group_variation_{$loop}",
+        'name'              => '_tai_packing_group_variation[' . $loop . ']',
+        'label'             => __( 'TAI Packing Group', 'tai-freight-shipping' ),
+        'value'             => $packing_group,
+        'type'              => 'number',
+        'desc_tip'          => true,
+        'description'       => __( 'PackingGroup enum value for this variation.', 'tai-freight-shipping' ),
+        'wrapper_class'     => 'form-row form-row-last',
+        'custom_attributes' => array(
+            'min'  => '0',
+            'step' => '1',
+        ),
+    ) );
+
+    woocommerce_wp_checkbox( array(
+        'id'            => "_tai_hazardous_material_variation_{$loop}",
+        'name'          => '_tai_hazardous_material_variation[' . $loop . ']',
+        'label'         => __( 'TAI Hazardous Material', 'tai-freight-shipping' ),
+        'value'         => ( 'yes' === $hazardous_material ) ? 'yes' : 'no',
+        'cbvalue'       => 'yes',
+        'description'   => __( 'Mark this variation as hazardous.', 'tai-freight-shipping' ),
+        'wrapper_class' => 'form-row form-row-full',
+        'desc_tip'      => false,
+        'class'         => 'checkbox',
+    ) );
+}
+
+/**
+ * Save TAI freight fields for each variation.
+ *
+ * @param int $variation_id Variation post ID.
+ * @param int $loop         Variation index.
+ */
+function tai_freight_save_variation_shipping_fields( $variation_id, $loop ) {
+    $nmfc = isset( $_POST['_tai_nmfc_variation'][ $loop ] )
+        ? sanitize_text_field( wp_unslash( $_POST['_tai_nmfc_variation'][ $loop ] ) )
+        : '';
+    update_post_meta( $variation_id, '_tai_nmfc', $nmfc );
+
+    $un_number = isset( $_POST['_tai_un_number_variation'][ $loop ] )
+        ? sanitize_text_field( wp_unslash( $_POST['_tai_un_number_variation'][ $loop ] ) )
+        : '';
+    update_post_meta( $variation_id, '_tai_un_number', $un_number );
+
+    $packaging_type = isset( $_POST['_tai_packaging_type_variation'][ $loop ] )
+        ? absint( wp_unslash( $_POST['_tai_packaging_type_variation'][ $loop ] ) )
+        : '';
+    update_post_meta( $variation_id, '_tai_packaging_type', ( '' === $packaging_type ) ? '' : (string) $packaging_type );
+
+    $packing_group = isset( $_POST['_tai_packing_group_variation'][ $loop ] )
+        ? absint( wp_unslash( $_POST['_tai_packing_group_variation'][ $loop ] ) )
+        : '';
+    update_post_meta( $variation_id, '_tai_packing_group', ( '' === $packing_group ) ? '' : (string) $packing_group );
+
+    $hazardous_material = isset( $_POST['_tai_hazardous_material_variation'][ $loop ] ) ? 'yes' : 'no';
+    update_post_meta( $variation_id, '_tai_hazardous_material', $hazardous_material );
 }
 
 /**
